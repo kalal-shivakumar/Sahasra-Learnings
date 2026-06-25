907e995e8c7466a2703d8d13020750f6068bb670
shivakumar kalal <shivakumar.kalal@marks-and-spencer.com>
2026-06-25 13:10:45 +0530

    Fix: Update package from ram09 to kalal and configure App Service

--- src/main/java/com/kalal/Application.java
+++ src/main/java/com/kalal/Application.java
@@ -1,4 +1,4 @@
-package com.ram09;
+package com.kalal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/kalal/controller/HomeController.java
+++ src/main/java/com/kalal/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ram09.controller;
+package com.kalal.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
